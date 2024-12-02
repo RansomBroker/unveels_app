6bf36cea37eb0486880e476bf9af6ee9f15f4d29
Muhammad Rizqi <muhammad-rizqi@live.com>
2024-12-02 10:33:09 +0700

    add to cart personality finder

--- lib/unveels_tech_evorty/features/personality_finder/presentation/widgets/pf_product_item_widget.dart
+++ lib/unveels_tech_evorty/features/personality_finder/presentation/widgets/pf_product_item_widget.dart
@@ -1,173 +1,297 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:test_new/unvells/app_widgets/app_alert_message.dart';
 import 'package:test_new/unvells/constants/app_routes.dart';
+import 'package:test_new/unvells/constants/app_string_constant.dart';
 import 'package:test_new/unvells/constants/arguments_map.dart';
+import 'package:test_new/unvells/helper/app_storage_pref.dart';
+import 'package:test_new/unvells/helper/utils.dart';
+import 'package:test_new/unvells/screens/home/bloc/home_screen_bloc.dart';
+import 'package:test_new/unvells/screens/home/bloc/home_screen_events.dart';
+import 'package:test_new/unvells/screens/home/widgets/item_card_bloc/item_card_bloc.dart';
+import 'package:test_new/unvells/screens/home/widgets/item_card_bloc/item_card_event.dart';
+import 'package:test_new/unvells/screens/home/widgets/item_card_bloc/item_card_state.dart';
 
 import '../../../../shared/configs/asset_path.dart';
 import '../../../../shared/widgets/buttons/button_widget.dart';
 import '../../../skin_tone_finder/skin_tone_product_model.dart';
 import '../../look_product_model.dart';
 
-class PFProductItemWidget extends StatelessWidget {
+class PFProductItemWidget extends StatefulWidget {
   const PFProductItemWidget({super.key, this.productData, this.lookData});
 
   final SkinToneProductData? productData;
   final LookProfiles? lookData;
+
+  @override
+  State<PFProductItemWidget> createState() => _PFProductItemWidgetState();
+}
+
+class _PFProductItemWidgetState extends State<PFProductItemWidget> {
+  ItemCardBloc? itemCardBloc;
+  bool isLoading = false;
+  bool _isAddingToCart = false;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.of(context).pushNamed(
-        AppRoutes.productPage,
-        arguments: getProductDataAttributeMap(
-          productData?.name ?? '',
-          productData?.id.toString() ?? '',
-        )
-      ),
-      child: SizedBox(
-        height: 242,
-        width: 151,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            lookData == null
-                ? CachedNetworkImage(
-                    imageUrl:
-                        "https://magento-1231949-4398885.cloudwaysapps.com/media/catalog/product${productData?.customAttributes.where((e) => e.attributeCode == 'small_image').first.value}",
-                    placeholder: (context, url) {
-                      return Container(
-                        color: Colors.white,
-                        child: const Center(
-                            child: SizedBox(
-                                height: 25,
-                                width: 25,
-                                child: CircularProgressIndicator())),
-                      );
-                    },
-                    errorWidget: (context, url, error) {
-                      return Container(
-                        color: Colors.white,
-                        child: const Center(
-                            child: SizedBox(
-                                height: 25, width: 25, child: Icon(Icons.error))),
-                      );
-                    },
-                    height: 242 * 0.65,
-                    fit: BoxFit.cover,
-                  )
-                : CachedNetworkImage(
-                    imageUrl:
-                        "https://magento-1231949-4398885.cloudwaysapps.com/media/${lookData?.image}",
-                    placeholder: (context, url) {
-                      return Container(
-                        color: Colors.white,
-                        child: const Center(
-                            child: SizedBox(
-                                height: 25,
-                                width: 25,
-                                child: CircularProgressIndicator())),
-                      );
-                    },
-                    errorWidget: (context, url, error) {
-                      return Container(
-                        color: Colors.white,
-                        child: const Center(
-                            child: SizedBox(
-                                height: 25, width: 25, child: Icon(Icons.error))),
-                      );
-                    },
-                    height: 242 * 0.65,
-                    fit: BoxFit.cover,
-                  ),
-            const SizedBox(
-              height: 3,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        lookData?.name ?? productData?.name ?? '-',
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w700,
+    itemCardBloc = context.read<ItemCardBloc>();
+    return BlocBuilder<ItemCardBloc, ItemCardState>(
+        builder: (context, currentState) {
+      if (currentState is ItemCardInitial) {
+        isLoading = true;
+      } else {
+        isLoading = false;
+        _isAddingToCart = false;
+        if (currentState is ItemCardErrorState) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            AlertMessage.showError(
+                currentState.message ??
+                    Utils.getStringValue(
+                        context, AppStringConstant.somethingWentWrong),
+                context);
+          });
+        } else if (currentState is AddToCartError) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            AlertMessage.showError(
+                currentState.message ??
+                    Utils.getStringValue(
+                        context, AppStringConstant.somethingWentWrong),
+                context);
+          });
+          itemCardBloc?.emit(ItemCardEmptyState());
+        } else if (currentState is AddProductToWishlistStateSuccess) {
+          if (currentState.wishListModel.success == true) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              AlertMessage.showSuccess(
+                  currentState.wishListModel.message ?? '', context);
+              itemCardBloc?.emit(WishlistIdleState());
+            });
+          } else {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              AlertMessage.showError(
+                  currentState.wishListModel.message ?? '', context);
+            });
+          }
+          itemCardBloc?.emit(ItemCardEmptyState());
+        } else if (currentState is AddtoCartState) {
+          if (currentState.model?.success == true) {
+            if ((currentState.model?.quoteId ?? 0) != 0) {
+              appStoragePref.setQuoteId(currentState.model?.quoteId);
+            }
+            appStoragePref.setCartCount(currentState.model?.cartCount);
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              AlertMessage.showSuccess(
+                  currentState.model?.message ?? "", context);
+              itemCardBloc?.emit(WishlistIdleState());
+            });
+          }
+        } else if (currentState is RemoveFromWishlistStateSuccess) {
+          if (currentState.baseModel.success == true) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              AlertMessage.showSuccess(
+                  currentState.baseModel.message ?? '', context);
+              itemCardBloc?.emit(WishlistIdleState());
+            });
+          } else {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              AlertMessage.showError(
+                  currentState.baseModel.message ??
+                      Utils.getStringValue(
+                          context, AppStringConstant.somethingWentWrong),
+                  context);
+            });
+          }
+          itemCardBloc?.emit(ItemCardEmptyState());
+          if (currentState.fromWishlist) {
+            Future.delayed(Duration.zero).then(
+              (value) {
+                HiveStore().reset();
+                context
+                    .read<HomeScreenBloc>()
+                    .add(const HomeScreenDataFetchEvent(true));
+              },
+            );
+          }
+        }
+      }
+      return GestureDetector(
+        onTap: () => Navigator.of(context).pushNamed(AppRoutes.productPage,
+            arguments: getProductDataAttributeMap(
+              widget.productData?.name ?? '',
+              widget.productData?.id.toString() ?? '',
+            )),
+        child: SizedBox(
+          height: 242,
+          width: 151,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              widget.lookData == null
+                  ? CachedNetworkImage(
+                      imageUrl:
+                          "https://magento-1231949-4398885.cloudwaysapps.com/media/catalog/product${widget.productData?.customAttributes.where((e) => e.attributeCode == 'small_image').first.value}",
+                      placeholder: (context, url) {
+                        return Container(
                           color: Colors.white,
+                          child: const Center(
+                              child: SizedBox(
+                                  height: 25,
+                                  width: 25,
+                                  child: CircularProgressIndicator())),
+                        );
+                      },
+                      errorWidget: (context, url, error) {
+                        return Container(
+                          color: Colors.white,
+                          child: const Center(
+                              child: SizedBox(
+                                  height: 25,
+                                  width: 25,
+                                  child: Icon(Icons.error))),
+                        );
+                      },
+                      height: 242 * 0.65,
+                      fit: BoxFit.cover,
+                    )
+                  : CachedNetworkImage(
+                      imageUrl:
+                          "https://magento-1231949-4398885.cloudwaysapps.com/media/${widget.lookData?.image}",
+                      placeholder: (context, url) {
+                        return Container(
+                          color: Colors.white,
+                          child: const Center(
+                              child: SizedBox(
+                                  height: 25,
+                                  width: 25,
+                                  child: CircularProgressIndicator())),
+                        );
+                      },
+                      errorWidget: (context, url, error) {
+                        return Container(
+                          color: Colors.white,
+                          child: const Center(
+                              child: SizedBox(
+                                  height: 25,
+                                  width: 25,
+                                  child: Icon(Icons.error))),
+                        );
+                      },
+                      height: 242 * 0.65,
+                      fit: BoxFit.cover,
+                    ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.lookData?.name ??
+                              widget.productData?.name ??
+                              '-',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "Brand name",
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 8,
-                          color: Colors.white.withOpacity(0.6),
+                        Text(
+                          "Brand name",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 8,
+                            color: Colors.white.withOpacity(0.6),
+                          ),
                         ),
+                      ],
+                    ),
+                  ),
+                  if (widget.lookData == null)
+                    Text(
+                      "\$${widget.productData?.price}",
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
                       ),
-                    ],
+                    ),
+                ],
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              SvgPicture.asset(
+                IconPath.fourStarsExample,
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: _isAddingToCart
+                        ? const Center(
+                            child: SizedBox(
+                              height: 24,
+                              width: 24,
+                              child: CircularProgressIndicator(
+                                  color: Colors.white),
+                            ),
+                          )
+                        : ButtonWidget(
+                            onTap: () async {
+                              Map<String, dynamic> mProductParamsJSON = {};
+                              itemCardBloc?.add(AddtoCartEvent(
+                                  widget.productData!.id.toString(),
+                                  1,
+                                  mProductParamsJSON));
+                              setState(() {
+                                _isAddingToCart = true;
+                              });
+                            },
+                            text: "ADD TO CART",
+                            backgroundColor: Colors.transparent,
+                            borderColor: Colors.white,
+                            height: 27,
+                            style: const TextStyle(
+                              fontSize: 8,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
                   ),
-                ),
-                if (lookData == null)
-                  Text(
-                    "\$${productData?.price}",
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white,
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Expanded(
+                    child: ButtonWidget(
+                      text: "SEE\nIMPROVEMENT",
+                      backgroundColor: Colors.white,
+                      height: 27,
+                      style: TextStyle(
+                        fontSize: 8,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 0,
+                      ),
                     ),
                   ),
-              ],
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            SvgPicture.asset(
-              IconPath.fourStarsExample,
-            ),
-            const Spacer(),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: ButtonWidget(
-                    text: "ADD TO CART",
-                    backgroundColor: Colors.transparent,
-                    borderColor: Colors.white,
-                    height: 27,
-                    style: TextStyle(
-                      fontSize: 8,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                  child: ButtonWidget(
-                    text: "SEE\nIMPROVEMENT",
-                    backgroundColor: Colors.white,
-                    height: 27,
-                    style: TextStyle(
-                      fontSize: 8,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      vertical: 0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
